f1277ce2535db15331cc621a78d8581e74473b64
Lars Hvam <larshp@hotmail.com>
2022-01-10 07:50:44 +0100

    verbose

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:current-alpine
+FROM node:lts-alpine
 
 RUN apk add --no-cache git
 RUN npm --version
--- src/index.ts
+++ src/index.ts
@@ -124,7 +124,7 @@ class Runner {
   private link() {
     const RUN_RESULT = "_link_result.txt";
     const start = Date.now();
-    execSync(`npm --global-style=true link @abaplint/runtime > ` + RUN_RESULT, {
+    execSync(`npm --loglevel verbose link @abaplint/runtime > ` + RUN_RESULT, {
       stdio: 'pipe',
       cwd: this.tmpDir });
     const end = Date.now();
